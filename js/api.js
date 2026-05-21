6029a36936ca4066070856027c40ed4694d795c9
SimAbild <abildtrup123@gmail.com>
2026-05-21 11:01:31 +0200

    ngrok til testing på til mobil

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 import { BACKEND_URL } from './env.js'
 
 export async function fetchRouteStops() {
-    const response = await fetch(`${BACKEND_URL}/driver/route`)
+    const response = await fetch(`${BACKEND_URL}/driver/route`, { headers: { 'ngrok-skip-browser-warning': 'true' } })
     if (!response.ok) throw new Error('Kunne ikke hente rute')
     return response.json()
 }
