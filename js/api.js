05ded56db4c49274c6a4bf3b7abdcc26950e749f
Joshua-kea <josv0002@stud.kea.dk>
2026-05-21 10:19:24 +0200

    merget med dev

--- js/api.js
+++ js/api.js
@@ -8,7 +8,6 @@ export async function sendPickupRequest(userId, bagCount) {
     })
     return response.ok
 }
-const BACKEND_URL = 'http://localhost:8080'
 
 export async function getActivePickupRequests() {
     const response = await fetch(`${BACKEND_URL}/active-pickup-requests`);
