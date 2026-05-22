1fe474453a3a4314478de674b2ca10e41a6fa9a9
SimAbild <abildtrup123@gmail.com>
2026-05-22 13:21:56 +0200

    fjernet test tingene til mobil, det er i en anden gren nu

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 import { BACKEND_URL } from './env.js'
 
 export async function fetchRouteStops() {
-    const response = await fetch(`${BACKEND_URL}/driver/route`, { headers: { 'ngrok-skip-browser-warning': 'true' } })
+    const response = await fetch(`${BACKEND_URL}/driver/route`)
     if (!response.ok) throw new Error('Kunne ikke hente rute')
     return response.json()
 }
--- js/env.js
+++ js/env.js
@@ -1,3 +1,1 @@
-// SLET DENNE FIL når du er færdig med mobil-test
-// export const BACKEND_URL = 'http://localhost:8080'
-export const BACKEND_URL = 'https://chip-clay-glacial.ngrok-free.dev'
+export const BACKEND_URL = 'http://localhost:8080'
