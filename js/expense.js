f2ec74310eb3ae214af8d077d698f42f2ceb323d
SimAbild <abildtrup123@gmail.com>
2026-05-26 11:00:10 +0200

    test telefon virker nu

--- js/expense.js
+++ js/expense.js
@@ -55,7 +55,7 @@ export async function expenseForm(){
         </div>
         <div class="expense-form-field">
     <label class="expense-file-label" for="expense-image">
-        Tilføj billede af kvitto
+        Tilføj billede af kvittering
     </label>
     <input type="file" id="expense-image" accept="image/*" required>
     <span class="expense-file-name" id="expense-file-name"></span>
